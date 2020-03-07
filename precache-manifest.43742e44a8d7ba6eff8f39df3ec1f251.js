a4ff6acbde5656fd83aeb9dcc6fb69c2fd905a4b
comfySheep <7jgjung@gmail.com>
2020-03-07 11:46:16 +0000

    Updates

---
+++ precache-manifest.43742e44a8d7ba6eff8f39df3ec1f251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22f6a53595d5b600b8710018262978c",
+    "url": "/gas-price-app/index.html"
+  },
+  {
+    "revision": "76d3ce41293c302a97b5",
+    "url": "/gas-price-app/static/css/main.3f688c14.chunk.css"
+  },
+  {
+    "revision": "6b6e9963a8fff2211fc8",
+    "url": "/gas-price-app/static/js/2.4592d105.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/gas-price-app/static/js/2.4592d105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d3ce41293c302a97b5",
+    "url": "/gas-price-app/static/js/main.41a8e5b3.chunk.js"
+  },
+  {
+    "revision": "884f78a967063f774696",
+    "url": "/gas-price-app/static/js/runtime-main.c9bd29fa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gas-price-app/precache-manifest.cd341709ed827d7b30d8da5722f2760e.js"
+  "/gas-price-app/precache-manifest.43742e44a8d7ba6eff8f39df3ec1f251.js"
 );
 
 self.addEventListener('message', (event) => {
